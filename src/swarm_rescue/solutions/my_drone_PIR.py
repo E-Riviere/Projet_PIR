5d3eebe795922d68fded5f3174833d5984e4202e
E-Riviere <enzo.riviere01@gmail.com>
2025-05-09 12:23:03 +0200

    Update

---
+++ src/swarm_rescue/solutions/my_drone_PIR.py
@@ -0,0 +1,132 @@
+from spg_overlay.utils.vortex_utils.Potential_field import PotentialField 
+from spg_overlay.entities.drone_abstract import DroneAbstract
+from spg_overlay.utils.vortex_utils.vortex_state_machines.sensors_analyzer import SensorsAnalyzer
+from spg_overlay.utils.vortex_utils.vortex_state_machines.actuators_calculator import ActuatorsComputer
+from spg_overlay.utils.misc_data import MiscData
+
+from typing import Optional
+
+import random
+
+import numpy as np
+
+import statistics
+
+def send_to_CF(x,y):
+    pass
+
+class MyDronePIR(DroneAbstract):
+
+    sensors_analyzer = None
+    actuators_computer = None
+    goal_data_analyze = None
+    choosed_gap = None
+    goal_compass = None
+    count = None
+
+    state = "take root"
+    def __init__(self,
+                identifier: Optional[int] = None,
+                misc_data: Optional[MiscData] = None,
+                **kwargs):
+        super().__init__(identifier=identifier,
+                        misc_data=misc_data,
+                        **kwargs)
+        
+        self.actuators_computer = ActuatorsComputer(identifier,self.lidar_rays_angles())
+        self.sensors_analyzer = SensorsAnalyzer(identifier)
+        self.sensors_analyzer.disable = False
+    def control(self):
+        x,y = self.gps_values()
+        send_to_CF(x,y)
+        
+        lidar_values = self.lidar_values()
+        self.returning_last_center = False
+        if self.state == "take root":
+            self.actuators_computer.take_root_control_command(self.gps_values(),(-200,0))
+        
+            if (x + 200)**2 + (y)**2 < 50:
+                self.state = "follow the gap"
+                print(self.state)
+                self.sensors_analyzer.disable = False
+        elif self.state == "follow the gap": 
+            print(self.sensors_analyzer.analyzed_data['positive gap number'])
+            if self.sensors_analyzer.analyzed_data['positive gap number'] > 0:
+                if self.sensors_analyzer.analyzed_data['positive gap number'] < 3 :
+                    if self.sensors_analyzer.analyzed_data['positive gap number'] == 1 and not self.returning_last_center:
+                        self.state = "align with gap"
+                        print(self.state)
+
+                        print(self.sensors_analyzer.analyzed_data['positive gap direction'])
+
+                        print(self.compass_values())
+                        print()
+                        pos_gap_dir = self.sensors_analyzer.analyzed_data['positive gap direction']
+                        self.choosed_gap = 0
+                        self.goal_compass = pos_gap_dir[self.choosed_gap]
+                        self.returning_last_center = True
+                    else:
+                        self.goal_data_analyze = self.sensors_analyzer.analyzed_data['positive gap direction']
+                        self.actuators_computer.FollowTheGap(self.goal_data_analyze,-1)
+                    
+                else:
+                    self.state = "center in intersection"
+                    print(self.state)
+                    
+                
+            
+        elif self.state == "center in intersection":
+            if self.goal_data_analyze:
+                self.actuators_computer.CenterInIntersection(self.sensors_analyzer.analyzed_data['negative gap dist ray'])
+                min_dist = []
+                self.returning_last_center = False
+                for obst in self.sensors_analyzer.analyzed_data['negative gap dist ray']:
+                    min_dist.append(obst[1])
+                
+                if statistics.variance(min_dist) < 5 and np.linalg.norm(self.measured_velocity()) < 0.1:
+                    self.state = "align with gap"
+                    print(self.state)
+                    self.choosed_gap = random.randint(0,self.sensors_analyzer.analyzed_data['positive gap number'] - 1)
+                    pos_gap_dir = self.sensors_analyzer.analyzed_data['positive gap direction']
+                    self.goal_compass = pos_gap_dir[self.choosed_gap]
+                    self.init_compass_value = self.compass_values()
+
+        elif self.state == "align with gap":
+            
+            
+            #self.choosed_gap = 0
+            #self.goal_compass = pos_gap_dir[self.choosed_gap]
+            #print(self.choosed_gap )
+                # gaps = self.sensors_analyzer.analyzed_data['positive gap direction'][choosed_gap]
+            self.actuators_computer.AlignWithTheGap(self.sensors_analyzer.analyzed_data['positive gap direction'],self.choosed_gap )
+            abs((abs(self.compass_values()) + abs(self.init_compass_value)) - abs(self.goal_compass))
+            print(self.compass_values())
+            print(self.goal_compass)
+            if abs((abs(self.compass_values()) + abs(self.init_compass_value)) - abs(self.goal_compass)) < 0.1 and self.measured_angular_velocity() < 0.1 :
+                self.state = "enter the gap"
+                print(self.state)
+                self.count = 0
+                
+
+
+            
+        
+        
+        if (x + 200)**2 + (y)**2 < 50:
+            self.initialized = True
+            self.sensors_analyzer.disable = False
+        self.sensors_analyzer.analyze(self.lidar_values(),self.lidar_rays_angles(),self.semantic_values())
+        command = self.actuators_computer.command
+        if self.state == "enter the gap":
+            self.count += 1
+            command = {
+                "forward" : 0.5,
+                "lateral" : 0.0,
+                "rotation" : 0.0
+            }
+            if self.count > 20:
+                self.state = "follow the gap"
+                print(self.state)
+        return command
+    def define_message_for_all(self):
+        return super().define_message_for_all()
--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -1,5 +1,5 @@
 from solutions.my_drone_random import MyDroneRandom
+from solutions.my_drone_PIR import MyDronePIR
 
-
-class MyDroneEval(MyDroneRandom):
+class MyDroneEval(MyDronePIR):
     pass
